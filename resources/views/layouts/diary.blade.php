fa0557528cda950778979727306040658f71885d
t-shibakusa <chibakk8@gmail.com>
2026-06-13 12:14:51 +0900

    不要機能の除去

--- resources/views/layouts/diary.blade.php
+++ resources/views/layouts/diary.blade.php
@@ -24,9 +24,6 @@
                         </svg>
                     </div>
                     <span class="font-medium text-diary-primary">{{ Auth::user()->name }}</span>
-                    <svg class="h-4 w-4 text-diary-muted" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                    </svg>
                 </div>
             </header>
 
